d01e789fdb181fc1cdfd06d1ae4f04d32f2d6707
Anastasiia Dovhoshiya <Dovgoshiya.uwo@gmail.com>
2022-10-20 20:56:05 +0300

    1 commit

---
+++ lab_4_1_alg/lab_4_1_alg.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int k, N;
+	double S;
+
+	cout << "k = "; cin >> k;
+	S = 1;
+	N = k;
+
+/*1*/
+	while (S <= 19)
+		S += ((k - N) / (k + N)) + 1;
+	cout << S << endl;
+	S = 1;
+
+/*2*/
+	do
+		S += ((k - N) / (k + N)) + 1;
+	while (S <= 19);
+	cout << S << endl;
+	S = 1;
+
+/*3*/
+	for(S = 1; S <= 19; )
+		S += ((k - N) / (k + N)) + 1;
+	cout << S << endl;
+
+
+
+
+
+
+}
